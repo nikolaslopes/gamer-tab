3202a89704b3bbc15a620618e4db72de7803dd03
nikolaslopes <nikolaslopes.dev@gmail.com>
2025-06-23 00:01:11 -0400

    chore: test

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
 function Home() {
+  const token =
+    '60IgiOUyPTUwF0o2G8oKMLWSvSiLYy84aDx88iei8vafGlBVhWq1su5u8It9wzSB';
+
+  console.log('Token:', token);
+
   return <h1>App in construction...</h1>;
 }
 
