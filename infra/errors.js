2eb9cff1efe78c81ac23c0adecd1ff8828900161
nikolaslopes <nikolaslopes.dev@gmail.com>
2025-10-21 17:04:36 -0400

    refactor: abstract router handlers

--- infra/errors.js
+++ infra/errors.js
@@ -1,11 +1,31 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super('An unexpected internal error occurred.', {
       cause,
     });
     this.name = 'InternalServerError';
     this.action = 'Contact support.';
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || 'Service currently unavailable.', {
+      cause,
+    });
+    this.name = 'ServiceError';
+    this.action = 'Check if the service is available.';
+    this.statusCode = 503;
   }
 
   toJSON() {
