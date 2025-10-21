3d6eef625f8752504ad0078953b60d879a31bf57
nikolaslopes <nikolaslopes.dev@gmail.com>
2025-10-21 16:00:59 -0400

    test: add `post` method coverage on `status` endpoint

--- infra/errors.js
+++ infra/errors.js
@@ -17,3 +17,22 @@ export class InternalServerError extends Error {
     };
   }
 }
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super('Method not allowed for this endpoint.');
+    this.name = 'MethodNotAllowedError';
+    this.action =
+      'Verify that the HTTP method sent is valid for this endpoint.';
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
